f16cd0884d4110fdc22b6062dd069d9bec182fc9
mluketin <mluketin@STUDENT06>
2019-11-14 21:31:07 +0100

    Done with the third exercise

---
+++ Vjezba3/src/main/java/Vjezba3_PUJ/App.java
@@ -0,0 +1,13 @@
+package Vjezba3_PUJ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
